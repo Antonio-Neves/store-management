3eaef159e1572f4be1cc55bca3d630094edabe70
Antonio-Neves <antoniocalheirosneves@gmail.com>
2026-07-18 14:31:39 -0300

    Changed CSS folder structure.

--- run_waitress.bat
+++ run_waitress.bat
@@ -2,7 +2,7 @@
 REM Script to be use for NSSM
 
 REM Need complete path
-call "D:\devdj\store_managementChanged \.venv\Scripts\activate.bat"
+call "D:\devdj\store_management\.venv\Scripts\activate.bat"
 
 REM Waitress start
 waitress-serve --port=8000 store_management.wsgi:application
